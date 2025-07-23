da1bb4272a53d04c3625759177bd10a7679bc0dc
Francisco Cordoba <franciscocordoba@Alaskas-MacBook.local>
2025-07-22 19:28:41 -0600

    Initial commit

---
+++ Counter/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Counter
+//
+//  Created by Francisco Cordoba on 22/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
